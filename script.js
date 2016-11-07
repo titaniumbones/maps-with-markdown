602e668b3d1afdcc9fdefb196846adce4f8d06d5
leejan16 <Janet@Janets-MacBook-Pro.local>
2016-11-06 23:36:06 -0500

    Think I'm Done...

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // whenever we need to -- they have 'global scope'
 var my_map; // this will hold the map
 var my_map_options; // this will hold the options we'll use to create the map
-var my_center = new google.maps.LatLng(43.649243, -78.365479); // center of map
+var my_center = new google.maps.LatLng(43.349243, -78.365479); // center of map
 var my_markers = []; // we use this in the main loop below to hold the markers
 // this one is strange.  In google maps, there is usually only one
 // infowindow -- its content and position change when you click on a
@@ -24,7 +24,7 @@ var blue_markers = [];
 function initialize() {
     my_map_options = {
         center:  my_center, // to change this value, change my_center above
-        zoom: 6,  // higher is closer-up
+        zoom: 7,  // higher is closer-up
         mapTypeId: google.maps.MapTypeId.HYBRID // you can also use TERRAIN, STREETMAP, SATELLITE
     };
 
@@ -37,33 +37,33 @@ function initialize() {
               map: my_map,
               icon: blueURL, // this sets the image that represents the marker in the map to the one
                              // located at the URL which is given by the variable blueURL, see above
-              title: "FortFrontenac",
+              title: " FortFrontenac",
               window_content: "<h3>Fort Frontenac</h3><p> Also called Fort Oswego by 18th century trader John Long- he describes this fort as being the key to the United States as it facilitated passageway to the North and to the Hudson River.</p>"
              },
              {position: new google.maps.LatLng(42.299369, -83.096096),
               map: my_map,
               icon: blueURL, // this sets the image that represents the marker in the map
-              title: "FortDetroit",
-              window_content: "<h3>Marker2</h3><p> and this would be the extended description</p>"
+              title: " FortDetroit",
+              window_content: "<h3> Fort Detroit</h3><p> An initially highly contentious fort, through agreements between the French and the Iroquois the fort was finally built. The French claimed that the fort would act as a place of arbitration for the hunting grounds surrounding it as well as provide the Iroquois with arms and provisions.</p>"
             },
             {position: new google.maps.LatLng(43.262389, -79.063116),
              map: my_map,
              icon: blueURL, // this sets the image that represents the marker in the map
-             title: "FortNiagara",
-             window_content: "<h3>Fort Niagara</h3><p> An important fort which would later come into British posession thanks to negotiations with the Seneca Native Indians. This fort was also the strategical supply point for the British and key for being able to quickly transport troops west in case of rebellion or war.</p>"
+             title: " FortNiagara",
+             window_content: "<h3> Fort Niagara</h3><p> An important fort which would later come into British posession thanks to negotiations with the Seneca Native Indians. This fort was also the strategical supply point for the British and key for being able to quickly transport troops west in case of rebellion or war.</p>"
 
            },
            {position: new google.maps.LatLng(42.652579, -73.756232),
             map: my_map,
             icon: redURL, // this sets the image that represents the marker in the map
-            title: "Albany",
-            window_content: "<h3>Albany</h3><p>Albany was a major trade hub where many Native groups such as the Iroquois and the western tribes often sold their pelts. The merchants at this post would often send agents to other forts such as Fort Frontenac in order to procure goods.  </p>"
+            title: " Albany",
+            window_content: "<h3> Albany</h3><p>Albany was a major trade hub where many Native groups such as the Iroquois and the western tribes often sold their pelts. The merchants at this post would often send agents to other forts such as Fort Frontenac in order to procure goods.  </p>"
           },
           {position: new google.maps.LatLng(41.866748, -77.838135),
            map: my_map,
            icon: redURL, // this sets the image that represents the marker in the map
            title: "NativeTerritory",
-           window_content: "<h3>Native Territories</h3><p>According to 18th century trader John Long, all land beneath Lake Ontario, Lake Erie, and the St. Lawrence River was claimed by the Five Nations Natives.</p>"
+           window_content: "<h3> Native Territories</h3><p>According to 18th century trader John Long, all land beneath Lake Ontario, Lake Erie, and the St. Lawrence River was claimed by the Five Nations Natives.</p>"
          },
          {position: new google.maps.LatLng(43.654460, -77.893066),
           map: my_map,
